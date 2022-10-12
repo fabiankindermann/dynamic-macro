3e8fcf3cf95ef91a3e987ed6cd27cededf60c915
Fabian Kindermann <fabian.kindermann@ur.de>
2022-10-12 19:10:40 +0200

    create file to install packages

---
+++ r_source/install_packages.R
@@ -0,0 +1,14 @@
+#######################################
+### Installs all packages that are needed for this course
+#######################################
+
+# Clear the workspace and install libraries
+rm(list = ls())
+install.packages("reshape2")
+install.packages("base")
+install.packages("ggplot2")
+install.packages("grid")
+install.packages("stringr")
+install.packages("tidyverse")
+install.packages("gridExtra")
+install.packages("pwt10")
